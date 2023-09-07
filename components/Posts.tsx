21430b62a1fb6429e54e418eec99aa483c36ac28
Yousef <yousefsameh24@gmail.com>
2023-09-07 18:32:29 +0300

    update posts component

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -1,19 +1,27 @@
 'use client'
 
 import usePosts from '@/hooks/use-posts';
+import PostsForm from '@/components/PostsForm';
+import { PostsContext } from '@/context/postsContext';
+import { useState } from 'react';
 
 export default function Posts() {
-    const { posts, isError, isLoading } = usePosts();
+    const { data, isError, isLoading } = usePosts();
+    const [posts, setPosts] = useState<any>(data || []);
+
     if (isError) return "An error has occurred.";
     if (isLoading) return "Loading...";
     return (
-      <div className="mx-auto w-fit p-12 mt-6 shadow-md rounded-xl border grid place-content-center">
-        <h1>Posts</h1>
-        <ul className="rounded-lg border shadow-sm p-8">
-          {posts?.map((post) => (
-            <li key={post.id}>{post.content}</li>
-          ))}
-        </ul>
-      </div>
+      <PostsContext.Provider value={{ posts, setPosts }}>
+        <div className="mx-auto w-fit p-12 mt-6 shadow-md rounded-xl border grid place-content-center">
+            <h1>Posts</h1>
+            <ul className="rounded-lg border shadow-sm p-8">
+            {data?.map((post) => (
+                <li key={post.id}>{post.content}</li>
+            ))}
+            </ul>
+            <PostsForm />
+        </div>
+      </PostsContext.Provider>
     )
 }
