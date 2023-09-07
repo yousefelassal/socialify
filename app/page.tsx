8c953819bfed2c42f4617df03a8e8a025af61de1
Yousef <yousefsameh24@gmail.com>
2023-09-07 16:59:03 +0300

    update page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import usePosts from '@/hooks/use-posts';
 
 export default function Home() {
@@ -5,9 +7,9 @@ export default function Home() {
   if (isError) return "An error has occurred.";
   if (isLoading) return "Loading...";
   return (
-    <div>
+    <div className="mx-auto w-fit p-12 mt-6 shadow-md rounded-xl border grid place-content-center">
       <h1>Posts</h1>
-      <ul>
+      <ul className="rounded-lg border shadow-sm p-8">
         {posts?.map((post) => (
           <li key={post.id}>{post.content}</li>
         ))}
