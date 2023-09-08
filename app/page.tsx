15f24f620f09c8155a59bec861295199a74ac2f3
Yousef <yousefsameh24@gmail.com>
2023-09-08 22:33:12 +0300

    update page margins

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Posts from "@/components/Posts";
 
 export default function Home() {
   return (
-    <div className="mx-auto w-fit p-12 mt-6 shadow-md rounded-xl border grid place-content-center">
+    <div className="mx-auto w-fit p-12 mt-20 shadow-md rounded-xl border grid place-content-center">
       <h1>Home</h1>
       <Posts />
     </div>
