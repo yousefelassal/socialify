c22f2f1d4f6f4603634dc3bf0bb13b6e2977d388
Yousef <yousefsameh24@gmail.com>
2023-09-09 00:23:45 +0300

    add images configs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ['localhost', 'res.cloudinary.com', 'raw.githubusercontent.com'],
+    }
+}
 
 module.exports = nextConfig
