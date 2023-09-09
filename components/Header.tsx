60f044151aa0637410201d46dbcfa9ae699ed2a2
Yousef <yousefsameh24@gmail.com>
2023-09-10 00:01:26 +0300

    update header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,9 @@
 import { useContext } from "react"
 import { UserContext } from "@/context/userContext"
-import ProfileDropdown from "./ProfileDropdown"
 import { Home, MessageCircle, Bell, Search, Users } from "lucide-react"
+
+import ProfileDropdown from "./ProfileDropdown"
+import Searchbar from "./Searchbar"
 
 export default function Header() {
   const { user, handleLogout } = useContext<any>(UserContext)
@@ -30,7 +32,7 @@ export default function Header() {
   return (
     <div className="flex justify-between items-center fixed bg-white/80 top-0 inset-x-0 py-4 pb-2 px-12 backdrop-blur-sm">
         <div className="flex items-center gap-8">
-            <div className="flex gap-2">
+            <div className="flex gap-2 items-center relative">
                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 130 130" fill="none">
                     <g clipPath="url(#clip0_17_11)">
                         <path d="M68.8518 13.7223V65.4815L57.0556 58.7407C36.1111 46.7037 23.1111 24.3148 23.1111 0H14.4445V59.2222C14.4445 78.4815 25.5185 96.2963 43.0926 104.722L69.0926 117.241V65.4815L80.8889 72.2222C101.833 84.2593 114.833 106.648 114.833 130.963H123.5V71.7407C123.5 52.4815 112.426 34.6667 94.8519 26.2407L68.8518 13.7223Z" fill="url(#paint0_linear_17_11)"/>
@@ -45,10 +47,7 @@ export default function Header() {
                     </clipPath>
                     </defs>
                 </svg>
-                <div className="relative flex items-center gap-2">
-                    <input className="bg-transparent border rounded-full px-4 py-1 border-blue-500/50 focus:outline-none focus:border-blue-500/80" placeholder="Search" />
-                    <Search className="absolute top-1/2 transform -translate-y-1/2 right-4 text-blue-500 h-5 w-5" />
-                </div>
+                <Searchbar />
             </div>
             <div className="flex gap-6">
                 {headerMenu.map((item, index) => (
