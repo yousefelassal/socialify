63328185efd649141c57b7c6de3724539ddb52d8
Yousef <yousefsameh24@gmail.com>
2023-09-11 13:33:30 +0300

    update posts

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -17,24 +17,18 @@ export default function Posts() {
     if (isLoading) return "Loading...";
     return (
       <PostsContext.Provider value={{ posts, setPosts }}>
-        <div className="mx-auto w-fit p-12 mt-6 shadow-md rounded-xl border grid place-content-center">
+        <div className="flex flex-col">
           <p>
             Welcome {user.name}! ({user.username})
           </p>
-            <h2>Posts</h2>
-            <ul className="rounded-lg border shadow-sm p-8">
-            {posts?.map((post: Post) => (
-                <li key={post.id}>{post.content}</li>
-            ))}
-            </ul>
-            <PostsForm />
+          <PostsForm />
+          <h2>Posts</h2>
+          <ul className="rounded-lg border shadow-sm p-8">
+          {posts?.map((post: Post) => (
+              <li key={post.id}>{post.content}</li>
+          ))}
+          </ul>
         </div>
-        <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
-            onClick={handleLogout}
-        >
-            Logout
-        </button>
       </PostsContext.Provider>
     )
 }
