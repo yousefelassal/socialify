854bdb74c0cd7ee4d932514898b1d356942c0011
Yousef <yousefsameh24@gmail.com>
2023-09-11 13:33:17 +0300

    update page

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,16 @@ import Posts from "@/components/Posts";
 
 export default function Home() {
   return (
-    <div className="mx-auto w-fit p-12 mt-20 shadow-md rounded-xl border grid place-content-center">
-      <h1>Home</h1>
-      <Posts />
+    <div className="p-12 mt-20 grid grid-cols-7">
+      <div className="col-span-2">
+        [NAV]
+      </div>
+      <div className="col-span-3">
+        <Posts />
+      </div>
+      <div className="col-span-2">
+        [Suggestions]
+      </div>
     </div>
   )
 }
