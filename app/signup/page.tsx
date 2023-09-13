93b4f50d78b8293e4cb2e8d8b272996efb0ce01f
Yousef <yousefsameh24@gmail.com>
2023-09-13 20:11:36 +0300

    update signup page

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -3,9 +3,6 @@ import SignUpForm from "@/components/SignUpForm";
 export default function page() {
   
   return (
-    <div className="mx-auto w-fit p-12 mt-6 shadow-md rounded-xl border grid place-content-center">
-      <h1>Sign Up</h1>
-      <SignUpForm />
-    </div>
+    <SignUpForm />
   )
 }
