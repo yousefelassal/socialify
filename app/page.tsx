27cfa5dfaf40694a75013739f03f30141b74194b
Yousef <yousefsameh24@gmail.com>
2023-09-13 20:50:18 +0300

    update margins

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Posts from "@/components/Posts";
 
 export default function Home() {
   return (
-    <div className="p-12 mt-20 grid grid-cols-7">
+    <div className="p-12 mt-16 grid grid-cols-7">
       <div className="col-span-2">
         [NAV]
       </div>
