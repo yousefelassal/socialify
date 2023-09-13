5d230104ae66f1539cd4c2f697b0b97e3807a191
Yousef <yousefsameh24@gmail.com>
2023-09-13 20:07:43 +0300

    add unsplash domain

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['localhost', 'res.cloudinary.com', 'raw.githubusercontent.com'],
+        domains: ['localhost', 'res.cloudinary.com', 'raw.githubusercontent.com', 'images.unsplash.com'],
     }
 }
 
