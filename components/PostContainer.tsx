9e1985717e3b52a39ba2a0645fd072d08d0f58da
Yousef <yousefsameh24@gmail.com>
2023-09-14 15:38:22 +0300

    update post container

--- components/PostContainer.tsx
+++ components/PostContainer.tsx
@@ -2,6 +2,18 @@ import { Post } from '@/types/Post';
 
 export default function PostContainer({post}: {post: Post}) {
   return (
-    <div>{post.content}</div>
+    <div className="p-4 border flex flex-col rounded-lg">
+      <div className="flex">
+        {post.content}
+      </div>
+      <div className="flex gap-4">
+        <div>
+          {post.likes}
+        </div>
+        <div>
+          {post.comments.length}
+        </div>
+      </div>
+    </div>
   )
 }
