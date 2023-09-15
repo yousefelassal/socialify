e67332fab4f61400c2e8ecead1ad73a788eb8753
Yousef <yousefsameh24@gmail.com>
2023-09-15 16:19:10 +0300

    update post layout

--- components/PostContainer.tsx
+++ components/PostContainer.tsx
@@ -1,8 +1,29 @@
 import { Post } from '@/types/Post';
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar"
 
 export default function PostContainer({post}: {post: Post}) {
   return (
-    <div className="p-4 border flex flex-col rounded-lg">
+    <div className="p-4 flex flex-col gap-4 bg-white/80 shadow-sm rounded-lg">
+      <div className="flex gap-2 items-center">
+        <div className="rounded-full w-10 h-10 grid place-content-center relative overflow-hidden">
+          <Avatar>
+              <AvatarFallback>{post.user.name.charAt(0)}</AvatarFallback>
+              <AvatarImage src={post.user.profile_pic} alt={post.user.name} />
+          </Avatar>
+        </div>
+        <div className="flex flex-col">
+          <div className="font-medium">
+            {post.user.name}
+          </div>
+          <div className="font-light">
+            @{post.user.username}
+          </div>
+        </div>
+      </div>
       <div className="flex">
         {post.content}
       </div>
@@ -11,7 +32,7 @@ export default function PostContainer({post}: {post: Post}) {
           {post.likes}
         </div>
         <div>
-          {post.comments.length}
+          {post.comments ? post.comments.length : 0}
         </div>
       </div>
     </div>
