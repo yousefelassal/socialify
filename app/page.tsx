c5504b4cc3789fb6e4ce8a6a221cc78e645bc80f
Yousef <yousefsameh24@gmail.com>
2023-09-29 16:43:15 +0300

    add nav & update cols distribution

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
 import Posts from "@/components/Posts";
+import Nav from "@/components/Nav";
 
 export default function Home() {
   return (
-    <div className="p-12 mt-16 grid grid-cols-7">
-      <div className="col-span-2">
-        [NAV]
-      </div>
+    <div className="p-12 mt-16 grid grid-cols-12">
       <div className="col-span-3">
+        <Nav />
+      </div>
+      <div className="col-span-6">
         <Posts />
       </div>
-      <div className="col-span-2">
+      <div className="col-span-3">
         [Suggestions]
       </div>
     </div>
